b4717ad054b7bd760d63b208fda6fb5160ef906c
Eric Sakmar <eric.sakmar@gmail.com>
2014-01-26 20:41:09 -0500

    updating example

--- examples/horiz/example.js
+++ examples/horiz/example.js
@@ -1,17 +1,14 @@
 (function() {
 
+  // scroll indicator
+  between(10).and(60).change('opacity').on('#indicator').from('1.0').to('0.0');
+  between(0).and(100).change('margin-top').on('#indicator').from('0px').to('300px');
+
   // opening
   between(0).and(500).change('height').on('header').from('50%').to('5%').then().between(1000).and(1500).go().from('5%').to('50%');
   between(0).and(500).change('height').on('footer').from('50%').to('5%').then().between(1000).and(1500).go().from('5%').to('50%');
 
-  // header
-  // between(0).and(500).change('margin-top').on('hgroup').from('30%').to('0%')
+  // github link
+  between(1600).and(1700).change('opacity').on('#github-link').from('0.0').to('1.0');
 
-  // scroll indicator
-  between(50).and(100).change('opacity').on('#indicator').from('1.0').to('0.0');
-  between(0).and(100).change('margin-top').on('#indicator').from('32px').to('300px');
-
-  // future ideas
-  // between(100).and(200).fix('top').on('EL ID')
-  // between(100).and(200).do(function (whatever) {})
 }).call(this);
